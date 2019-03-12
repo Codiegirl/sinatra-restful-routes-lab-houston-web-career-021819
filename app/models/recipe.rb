9caf55886f40e476a1cb3366cf4bc764c94c8520
Rochell Flood <iroflood@msn.com>
2019-03-12 15:12:47 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,10 @@
 #Placeholder for a model
 class Recipe
   attr_accessor :name, :ingredients, :cook_time
-  
+  @@all = []
   
   def initialize(name,ingredients,cook_time)
     self.name = name
     self.ingredients = ingredients
     self.cook_time = cook_time
+    @@all << self
