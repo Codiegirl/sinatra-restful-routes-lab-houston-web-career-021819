b189acd30f5143cb60ce79f333852b49e723785d
Rochell Flood <iroflood@msn.com>
2019-03-12 15:20:44 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,10 +3,8 @@ class Recipe<ActiveRecord::Base
   attr_accessor :name, :ingredients, :cook_time
   @@all = []
   
-  def initialize(name,ingredients,cook_time)
+  def initialize(name)
     self.name = name
-    self.ingredients = ingredients
-    self.cook_time = cook_time
     @@all << self
   end 
   
